357f61a6efead687eb4df32e08b8568b1847cfc2
VukasinVranic <vukasinvranic@gmail.com>
2017-05-19 17:19:58 +0200

    Za sad imamo, Chat i on šljaka, idemo dalje sad!

--- src/main/Klijent.java
+++ src/main/Klijent.java
@@ -1,31 +1,65 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Klijent {
-	static int port;
+public class Klijent implements Runnable {
+
 	static Socket soketZaKomunikaciju = null;
-	static PrintStream izlazKaServeru = null;
-	static BufferedReader ulazKaServeru = null;
-	static BufferedReader konzola = null;
-	
+
+	static PrintStream izlazniTokKaServeru = null;
+	static BufferedReader ulazniTokOdServera = null;
+
+	static BufferedReader ulazKonzola = null;
+
+	static boolean kraj = false;
+
 	public static void main(String[] args) {
 		try {
-			
-		soketZaKomunikaciju = new Socket("localhost", port);
-		konzola = new BufferedReader(new InputStreamReader(System.in));
-		ulazKaServeru = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
-		izlazKaServeru = new PrintStream(soketZaKomunikaciju.getOutputStream());
-		// e sad ovde treba da nastane komunikacija
-		// i treba da se pozovu metode iz klase "Igra"
-		
-		
-		
-		} catch (Exception e) {
-			System.out.println("Negde si se ukanalio!");
+			int port = 9999;
+
+			if (args.length > 0) {
+				port = Integer.parseInt(args[0]);
+			}
+
+			soketZaKomunikaciju = new Socket("localhost", port);
+
+			ulazKonzola = new BufferedReader(new InputStreamReader(System.in));
+
+			izlazniTokKaServeru = new PrintStream(soketZaKomunikaciju.getOutputStream());
+			ulazniTokOdServera = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
+
+			new Thread(new Klijent()).start();
+
+			while (!kraj) {
+				izlazniTokKaServeru.println(ulazKonzola.readLine());
+			}
+
+			soketZaKomunikaciju.close();
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host ");
+		} catch (IOException e) {
+			System.err.println("IOException: " + e);
+		}
+	}
+
+	public void run() {
+		String linijaOdServera;
+		try {
+			while ((linijaOdServera = ulazniTokOdServera.readLine()) != null) {
+				System.out.println(linijaOdServera);
+
+				if (linijaOdServera.indexOf("*** Dovidjenja") == 0) {
+					kraj = true;
+					return;
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("IOException: " + e);
 		}
 	}
 }
