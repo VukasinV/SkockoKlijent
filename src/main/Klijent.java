6c0ff52b6fa3532d04beb104843287d10d314c2a
VukasinVranic <vukasinvranic@gmail.com>
2017-05-22 19:58:03 +0200

    aaaaaaaaaaaaaaaaaaaa

--- src/main/Klijent.java
+++ src/main/Klijent.java
@@ -1,60 +1,69 @@
 
-	package main;
+package main;
 
-	import java.io.BufferedReader;
-	import java.io.IOException;
-	import java.io.InputStreamReader;
-	import java.io.PrintStream;
-	import java.net.Socket;
-	import java.net.UnknownHostException;
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-	/*impelentira se kao Runnable da bi se mogla pokrenuti kao nit*/
-	public class Klijent implements Runnable {
-		static Socket soketZaKomunikaciju = null;
-		static PrintStream izlazniTokKaServeru = null;
-		static BufferedReader ulazniTokOdServera = null;
-		static BufferedReader ulazKonzola = null;
-		
-		/*da li je korisnik izasao iz chata*/
-		static boolean kraj = false;
+import javax.swing.JDialog;
 
-		public static void main(String[] args) {
-			try{
-				int port = 2222;
-				if(args.length>0){
-					port = Integer.parseInt(args[0]);
-				}
-				/*povezivanje na host*/
-				soketZaKomunikaciju = new Socket("localhost",port);
-				ulazKonzola = new BufferedReader(new InputStreamReader(System.in));
-				izlazniTokKaServeru = new PrintStream(soketZaKomunikaciju.getOutputStream());
-				ulazniTokOdServera = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
-				/*pravi se nit koja ce citati poruke*/
-				new Thread(new Klijent()).start();
-				while(!kraj){
-					/*pise poruke serveru,dokle god nije kraj, liniju po liniju sa konzole*/
-					izlazniTokKaServeru.println(ulazKonzola.readLine());
-				}
-				soketZaKomunikaciju.close();
-			}catch(UnknownHostException e){
-				System.err.println("DONT KNOW ABOUT HOST "+e);
-			}catch(IOException e){
-				System.err.println("IOException :" + e);
+public class Klijent extends Window implements Runnable {
+	static Socket soketZaKomunikaciju = null;
+	static PrintStream izlazniTokKaServeru = null;
+	static BufferedReader ulazniTokOdServera = null;
+	static BufferedReader ulazKonzola = null;
+
+	static boolean kraj = false;
+
+	public static void main(String[] args) {
+		try {
+			int port = 2222;
+			if (args.length > 0) {
+				port = Integer.parseInt(args[0]);
 			}
-		}
-		public void run(){
-			String linijaOdServera;
-			try{
-				while((linijaOdServera = ulazniTokOdServera.readLine())!= null){
-					System.out.println(linijaOdServera);
-					if(linijaOdServera.indexOf("*** Dovidjenja")==0){
-						kraj = true;
-						return;
-					}
-				}
-			}catch(IOException e){
-				System.err.println("IOException "+ e);
+			soketZaKomunikaciju = new Socket("localhost", port);
+			ulazKonzola = new BufferedReader(new InputStreamReader(System.in));
+			izlazniTokKaServeru = new PrintStream(soketZaKomunikaciju.getOutputStream());
+			ulazniTokOdServera = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
+			new Thread(new Klijent()).start();
+			Window window = new Window();
+			Login login = new Login();
+			login.setVisible(true);
+			//window.setVisible(true);
+			while (!kraj) {
+				//izlazniTokKaServeru.println(ulazKonzola.readLine());
 			}
+			soketZaKomunikaciju.close();
+		} catch (UnknownHostException e) {
+			System.err.println("DONT KNOW ABOUT HOST " + e);
+		} catch (IOException e) {
+			System.err.println("IOException :" + e);
 		}
-
 	}
+
+	public void run() {
+		String linijaOdServera;
+		try {
+			while ((linijaOdServera = ulazniTokOdServera.readLine()) != null) {
+				System.out.println(linijaOdServera);
+				if (linijaOdServera.indexOf("***") == 0) {
+					kraj = true;
+					return;
+				}
+				
+				if (linijaOdServera.startsWith("Opet") || linijaOdServera.startsWith("Ime")) {
+					Login.odgovor = linijaOdServera;
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("IOException " + e);
+		}
+	}
+
+}
